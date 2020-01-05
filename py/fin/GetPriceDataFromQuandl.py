56bbd3c5fa16be04e0d3b6a022db3c048170a3bb
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-04 23:10:01 -0500

    added matplotlib to the quandl and yfinance py scripts

--- py/fin/GetPriceDataFromQuandl.py
+++ py/fin/GetPriceDataFromQuandl.py
@@ -1,8 +1,27 @@
 # =============================================================================
 # Prerequisites:
-#   1. NumPy (v1.8 or above) 
-#   2. Pandas (v0.14 or above) 
-#   3. An API key from Quandl
+#   1. quandl
+#   2. matplotlib
+#   3. An API key from https://www.quandl.com/
+
+# Install the prerequisites
+#   1. launch a CMD prompt with the 'Run as Administrator' option
+#   2. python -m pip install --upgrade pip
+#   3. pip install quandl
+#   4. pip install matplotlib
+
+# Help Notes
+#   pip list
+#   pip search <module>
+#   pip search quandl
 # =============================================================================
 
-import quandl
+# auth_tok = "<your-api-key-here>";
+
+import quandl as ql;
+data = ql.get("WIKI/MSFT", start_date="2000-01-01", end_date="2019-12-31", authtoken=auth_tok);
+print(data);
+
+import matplotlib.pyplot as plt;
+data.Close.plot();
+plt.show();
--- py/fin/GetStockPriceUsingYFinance.py
+++ py/fin/GetStockPriceUsingYFinance.py
@@ -1,5 +1,27 @@
+# =============================================================================
+# Prerequisites
+#   1. yfinance
+#   2. matplotlib
+
+# Install the prerequisites
+#   1. launch a CMD prompt with the 'Run as Administrator' option
+#   2. python -m pip install --upgrade pip
+#   3. pip install yfinance
+#   4. pip install matplotlib
+
+# Help Notes
+#   pip list
+#   pip search <module>
+#   pip search yfinance
+# =============================================================================
+
 import yfinance as yf;
-# if error, 
-    # 1. launch a CMD prompt with the 'Run as Administrator' option
-    # 2. python -m pip install --upgrade pip
-    # 2. pip install yfinance
+# msft = yf.Ticker("MSFT");
+# data = msft.info;
+# data = yf.download("MSFT", start="2000-01-01", end="2019-12-31")
+data = yf.download("FB AAPL AMZN NFLX GOOGL MSFT", start="2000-01-01", end="2019-12-31")
+print(data);
+
+import matplotlib.pyplot as plt;
+data.Close.plot();
+plt.show();
