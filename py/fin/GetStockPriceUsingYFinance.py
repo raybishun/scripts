974e234af52ea105f6d26ae44286c4ce1df0a0f7
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-04 20:51:45 -0500

    Restructured folders

---
+++ py/fin/GetStockPriceUsingYFinance.py
@@ -0,0 +1,5 @@
+import yfinance as yf;
+# if error, 
+    # 1. launch a CMD prompt with the 'Run as Administrator' option
+    # 2. python -m pip install --upgrade pip
+    # 2. pip install yfinance
