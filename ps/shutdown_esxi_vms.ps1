f2ac775393825ac822e2d46628ec56a5f583df5d
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-01-17 21:15:46 -0500

    Added shutdown_esxi_vms.ps1

---
+++ ps/shutdown_esxi_vms.ps1
@@ -0,0 +1,26 @@
+# =============================================================================
+# Shutdown ESXi Lab VMs
+# =============================================================================
+Clear-Host
+
+$computers = @("vmcentos8splunk", "vmw2k19eval1", "vmw2k16sccm19", "vmw2k12r2sql16", "vmw2k12r2dc1", "vmw2k12r2dc0")
+
+$admin = Get-Credential bitsbyteslab\ray.bishun
+
+foreach($computer in $computers)
+{
+    Write-Host $computer
+
+    try
+    {
+        Stop-Computer -ComputerName $computer -Force -Credential $admin -ErrorAction Stop
+        
+        Start-Sleep -s 120
+    }
+    catch
+    {
+        Write-Host $_.Exception.Message
+    }
+}
+
+Write-Host "Done."
