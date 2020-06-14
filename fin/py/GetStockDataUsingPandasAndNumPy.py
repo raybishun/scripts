13037a52e0b77b02cb4f5d82e505f4a550dbc0e6
ray.bishun@bitsbytes.com <ray.bishun@bitsbytes.com>
2020-06-13 21:29:09 -0400

    Working with Pandas and NumPy

---
+++ fin/py/GetStockDataUsingPandasAndNumPy.py
@@ -0,0 +1,14 @@
+# Dependencies
+# 1. pip insall pandas-datareader
+# 2. pip install matplotlib
+# 3. pip install pandas
+# 4. pip install numpy
+# 5. pip install datetime
+# Troubleshooting
+# 1. Ctrl + Shift + P to change the Python Interpreter
+# 2. python -m pip install --upgrade pip
+
+# import io
+# from io import StringIO
+from pandas_datareader import data
+from pandas_datareader.utils import RemoteDataError
